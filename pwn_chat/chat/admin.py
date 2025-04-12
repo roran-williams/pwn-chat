1e38dbea72d27d39e0d9559e0fd1f6983a82d0ed
rtox3dragon <rtox3dragon@gmail.com>
2025-04-12 09:42:40 +0300

    added rooms in models.

--- pwn_chat/chat/admin.py
+++ pwn_chat/chat/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Message
+from .models import Message, Room, Status
 
 # Register your models here.
 @admin.register(Message)
@@ -7,3 +7,17 @@ class MessageAdmin(admin.ModelAdmin):
     list_display = ('user', 'text', 'timestamp')
     search_fields = ('user', 'text', 'timestamp')
     list_filter = ('user', 'text', 'timestamp')
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'desc', 'status','creation_time','update_time','created_by')
+    search_fields =  ('name', 'desc', 'status','creation_time','update_time','created_by')
+    list_filter = ('name', 'desc', 'status','creation_time','update_time','created_by')
+
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_default', 'hide_by_default')
+    search_fields =  ('name', 'is_default', 'hide_by_default')
+    list_filter = ('name', 'is_default', 'hide_by_default')
--- pwn_chat/chat/models.py
+++ pwn_chat/chat/models.py
@@ -1,10 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=32)
+    is_default = models.BooleanField(default=False)
+    hide_by_default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class Room(models.Model):
+    name = models.CharField(max_length=28,null=False,default='pwn')
+    desc = models.TextField(null=True)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, null=True)  # Use ForeignKey to Status
+
+    creation_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now_add=True)
+
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, related_name='room_created', on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name 
 
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE,null=True)
     text = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.user.username}: {self.text[:20]}"
+
